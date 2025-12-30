8d79287c7d5db8d786fa99fece6db56d17fe7ca4
umesh rajput <raputumesh117@gmail.com>
2025-12-30 15:46:36 +0530

    update

---
+++ Practice/Scripts/script.js
@@ -0,0 +1,25 @@
+// function show (){
+//  alert("Hey Nisha");
+// }
+
+// const button = document.querySelector("#btn");
+
+// button.onclick = function show(){
+//     alert("Hey Nisha");
+// }
+
+// button.addEventListener("click",function hii(){
+//     alert("Hey Nisha JI")
+// });
+
+// button.addEventListener("click",function(event){
+//     console.log(event.type);
+//     console.log(event.target)
+// })
+
+// function hello(){
+//     alert("Nisha")
+// };
+// const button1 = document.querySelector("#btn1");
+// button1.addEventListener("click",hello);
+// button1.removeEventListener("click",hello);
